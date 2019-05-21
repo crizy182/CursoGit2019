25560a2ed59439383dcd0785c81884f1e462ecb6
crizy182 <cristhiam.tovar@mail.escuelaing.edu.co>
2019-05-21 14:09:00 -0500

    change tall added

---
+++ toPrint.bash
@@ -0,0 +1,21 @@
+Vicky@LENOVO-PC MINGW64 ~/Documents/platzi/gitHub/GitHub2019 (master)
+$ git show history.txt
+commit 3c3782256243571f1802fe231de3f5a94ca1aa89 (HEAD -> master)
+Author: crizy182 <cristhiam.tovar@mail.escuelaing.edu.co>
+Date:   Tue May 21 12:07:45 2019 -0500
+
+    changes to history file for correct grammar
+
+diff --git a/history.txt b/history.txt
+index 2bd451b..d5032aa 100644
+--- a/history.txt
++++ b/history.txt
+@@ -1,5 +1,6 @@
+ This is the Freddy Vega's history
+
+-Freddy Vega is 32 years old and he born in Colombia
++Freddy Vega is 33 years old and he borns in Colombia,
++living in the whole world
+
+ we'll talk about his history.
+\ No newline at end of file
